637ee289b047c92d2a333387262fd1a88338afd8
ziktapurik <ziktapurik@gmail.com>
2020-10-29 12:32:34 +0300

    ex. 1.4. Добавил комментарии

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,8 +4,8 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-    str = str.toLowerCase();
-    if ( str.indexOf("1xbet")!=-1 || str.indexOf("xxx")!=-1 ) {
+    str = str.toLowerCase(); //переводим все в нижний регистр для регистровой независимости
+    if ( str.indexOf("1xbet")!=-1 || str.indexOf("xxx")!=-1 ) { //проверка на наличие спама
         return true;
     } else return false;
 }
