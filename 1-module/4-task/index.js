2493f380603eb6a5cfd3191cc5b80915e692a884
ziktapurik <ziktapurik@gmail.com>
2020-10-29 12:29:39 +0300

    ex. 3.1. проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,8 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+    str = str.toLowerCase();
+    if ( str.indexOf("1xbet")!=-1 || str.indexOf("xxx")!=-1 ) {
+        return true;
+    } else return false;
 }
