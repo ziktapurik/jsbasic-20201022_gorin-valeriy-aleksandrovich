28c0dba71d3c3a0374763ae076ae76d59793d9ed
ziktapurik <ziktapurik@gmail.com>
2020-11-02 17:03:06 +0300

    ex. 3.1 калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,10 @@
 let calculator = {
-  // ваш код
+    read(a, b) { //записываем данные в объект
+        this.number1 = a;
+        this.number2 = b;
+    },
+    sum() {return this.number1 + this.number2}, //складываем данные из объекта
+    mul() {return this.number1 * this.number2}, // умножаем данные из объекта
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
