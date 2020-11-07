9caeacd755ce36e1edd8670d2fa45576cafe3701
ziktapurik <ziktapurik@gmail.com>
2020-11-07 14:39:57 +0300

    ex. 3.2. Фильтрация по диапазону

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -5,5 +5,18 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
-  // ваш код...
+    let min, max;
+	if (a >= b) { //по условию a может быть больше b и наоборот
+        max = a;
+        min = b;
+    } else {
+        max = b;
+        min = a;
+    }
+	
+	let total=[];
+    for (let i of arr) {
+        if ((i>=min) && (i<=max)) {total.push(i)}; //Если элемент массива arr попадает в диаппазон между min и max, добавляем его в total
+    }
+	return total;
 }
