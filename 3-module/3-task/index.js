c7f48039ba81ee4488c85b2177d3250a9f2f52d6
ziktapurik <ziktapurik@gmail.com>
2020-11-07 16:09:09 +0300

    ex. 3.3. Переведите текст вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,10 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+    let arr = str.split("-"); //разделяем строку по словам
+	
+    for (let i=1; i<arr.length; i++) {
+        arr[i]=arr[i][0].toUpperCase() + arr[i].slice(1); //всем словам кроме первого делаем первую букву заглавной
+    }
+	return arr.join('');
 }
