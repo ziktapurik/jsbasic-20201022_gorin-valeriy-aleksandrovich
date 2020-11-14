19494ebd9970955a71c51a92fe935d0de0c6aeff
ziktapurik <ziktapurik@gmail.com>
2020-11-14 21:55:00 +0300

    ex.4.3. Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,51 @@
  * @param {Element} table
  */
 function highlight(table) {
+    
+    // Ищем индексы нужных нам столбцов таблицы
+    let age;
+    let gender;
+    let status;
+    for (let columns of table.tHead.rows[0].cells) { 
+               
+        if (columns.textContent === 'Age') {
+            age=columns.cellIndex;
+        } else if (columns.textContent === 'Gender')  {
+            gender=columns.cellIndex;
+        } else if (columns.textContent === 'Status')  {
+            status=columns.cellIndex;
+        }
+        
+    }
+    
+    
+    for (let bodies of table.tBodies) {
+       for (let tr of bodies.rows) {
+           console.log(tr.cells[status]);
+           
+           //добавляем класс available/unavailable/hidden
+           if (tr.cells[status].getAttribute('data-available') == 'true') {
+               tr.classList.add('available');
+           } else if (tr.cells[status].getAttribute('data-available') == 'false') {
+               tr.classList.add('unavailable'); 
+           } else if (!tr.cells[status].hasAttribute('data-available')) {
+               tr.setAttribute('hidden', true);
+           }
+           
+           // добавляем класс male/female
+           if (tr.cells[gender].textContent === 'm') {
+               tr.classList.add('male');
+           } else if (tr.cells[gender].textContent === 'f') {
+               tr.classList.add('female');
+           }
+           
+           //добавляем inline-стиль style="text-decoration: line-through"
+           if (tr.cells[age].textContent < 18) {
+               tr.style="text-decoration: line-through";
+           }
+        
+        }
+        console.log(bodies);
+    }
+    
 }
