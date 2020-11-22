aacc8e37a2cbf4a2313db16e1d38747c9f542053
ziktapurik <ziktapurik@gmail.com>
2020-11-22 11:35:44 +0300

    ex. 5.2. Скрыть элемент по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+    const hideText = document.querySelector('.toggle-text-button');
+    hideText.addEventListener('click', () => {
+        const toggleText = document.querySelector('#text');
+        if (toggleText.hasAttribute('hidden', true)) { //Если текст скрыт - показываем его, если текст отображается - наоборот, скрываем
+            toggleText.removeAttribute('hidden', true);
+        } else {
+            toggleText.setAttribute('hidden', true);
+        }
+    });
 }
