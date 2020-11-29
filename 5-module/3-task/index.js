0e9c9db6f7cf83a25341a0987ebafbf09e17d9ba
ziktapurik <ziktapurik@gmail.com>
2020-11-29 15:26:03 +0300

    ex. 5.3 Учебный проект (пролог): Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,51 @@
 function initCarousel() {
-  // ваш код...
+    
+    function displayArrow() { //скрывает стрелку влево на первом слайде и стрелку вправо на последнем      
+        if ( slideNumber == 1 ) {
+            arrowLeft.style.display = 'none'
+        } else {
+            arrowLeft.style.display = '';
+        }
+        if ( slideNumber == picsCount ) {
+            arrowRight.style.display = 'none'
+        } else {
+            arrowRight.style.display = '';
+        }
+        
+    }
+    function movement(direction) { //переключает слайды
+        let translateX;
+        if ( direction == 'left' ) {
+            translateX = width*slideNumber;
+            slideNumber++
+        } else if ( direction == 'right' ) {
+            slideNumber--;
+            translateX = width*(slideNumber-1);
+        }
+        return translateX;
+    }
+    
+
+    const carousel = document.querySelector('.carousel');
+    const carouselMove = carousel.querySelector('.carousel__inner');
+    const pics = carousel.querySelectorAll(".carousel__slide");
+    const picsCount = pics.length; //количество картинок в карусели
+    const arrowLeft = carousel.querySelector('.carousel__arrow_left');
+    const arrowRight = carousel.querySelector(".carousel__arrow_right");
+    const width = carouselMove.offsetWidth; // получаем ширину слайда
+    let slideNumber = 1; //номер текущего слайда
+    displayArrow();
+    
+    
+    carousel.addEventListener('click', () => {
+        if (event.target.closest('div').className === "carousel__arrow carousel__arrow_right") {
+            carouselMove.style.transform = `translateX(-${movement('left')}px)`;
+            displayArrow() 
+            
+        } else if (event.target.closest('div').className === "carousel__arrow carousel__arrow_left") {           
+            carouselMove.style.transform = `translateX(-${movement('right')}px)`;
+            displayArrow()
+            
+        } 
+    });
 }
